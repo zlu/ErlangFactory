88fabc8b6a4135c9ef2bf1b4f9a9aceb8fc3f6f1
zhao lu <zlu@me.com>
2012-03-27 11:23:51 -0700

    Added mergesort skeleton

--- sort.erl
+++ sort.erl
@@ -1,6 +1,9 @@
 -module(sort).
--export([quick/1]).
+-export([quick/1, merge/1]).
 
 quick([]) -> [];
 quick([Pivot | T]) ->
     quick(more_list:filter(T, Pivot)) ++ [Pivot] ++ quick(more_list:filter2(T, Pivot)).
+
+merge([]) -> [];
+merge() -> 
